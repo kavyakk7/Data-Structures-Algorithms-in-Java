1e728375fdba22382e320202bba60f3fd7e1e0f2
Kavya Kesavan <kavyakk7@users.noreply.github.com>
2018-09-16 14:31:35 -0500

    Update BubbleSort.java

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -5,19 +5,24 @@ package sorting;
  */
 public class BubbleSort {
 
-	public static void bubbleSort(int[] arr) {
-		for(int i = 0; i < arr.length - 1 ; i++) {
-			for(int j = i+1; j < arr.length; j++) {
-				if(arr[i]>arr[j]) {
-					int temp = arr[i];
-					arr[i] = arr[j];
-					arr[j] = temp;
+	public static void bubbleSort(int[] array) {
+		boolean isSorted = false;
+		int temp, lastUnsorted = array.length - 1;
+		while(!isSorted) {
+			isSorted = true;
+			for(int i = 0; i < lastUnsorted; i++) {
+				if(array[i] > array[i+1]) {
+					temp = array[i];
+					array[i] = array[i+1];
+					array[i+1] = temp;
+					isSorted = false;
 				}
-			}
+			}	
+			lastUnsorted--;
 		}
 	}
 	public static void main(String[] args) {
-		int[] arr = {36,54,65,91,11,23};
+		int[] arr = {36,54,65,91,11,23,1};
 		bubbleSort(arr);
 		for(int i = 0; i < arr.length ; i++)
 			System.out.print(arr[i]+" ");
